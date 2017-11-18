cace6a91547b744a870c4db8f2bf3465e3177c94
Jonah Kanner <jkanner@caltech.edu>
2017-11-17 16:04:51 -0800

    First import

---
+++ get_datafiles.sh
@@ -0,0 +1,19 @@
+
+wget https://losc.ligo.org/s/events/BBH_events_v3.json
+wget https://losc.ligo.org/s/sample_code/readligo.py
+
+wget https://losc.ligo.org/s/events/GW150914/H-H1_LOSC_4_V2-1126259446-32.hdf5
+wget https://losc.ligo.org/s/events/GW150914/L-L1_LOSC_4_V2-1126259446-32.hdf5
+wget https://losc.ligo.org/s/events/GW150914/GW150914_4_template.hdf5
+
+wget https://losc.ligo.org/s/events/LVT151012/H-H1_LOSC_4_V2-1128678884-32.hdf5
+wget https://losc.ligo.org/s/events/LVT151012/L-L1_LOSC_4_V2-1128678884-32.hdf5
+wget https://losc.ligo.org/s/events/LVT151012/LVT151012_4_template.hdf5
+
+wget https://losc.ligo.org/s/events/GW151226/H-H1_LOSC_4_V2-1135136334-32.hdf5
+wget https://losc.ligo.org/s/events/GW151226/L-L1_LOSC_4_V2-1135136334-32.hdf5
+wget https://losc.ligo.org/s/events/GW151226/GW151226_4_template.hdf5
+
+wget https://losc.ligo.org/s/events/GW170104/H-H1_LOSC_4_V1-1167559920-32.hdf5
+wget https://losc.ligo.org/s/events/GW170104/L-L1_LOSC_4_V1-1167559920-32.hdf5
+wget https://losc.ligo.org/s/events/GW170104/GW170104_4_template.hdf5
